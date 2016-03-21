4c18c503b34cef83f7ee48f0030324c9bc0ef665
Mrigesh <mrigesh@cmu.edu>
2016-03-21 18:59:16 +0000

    Created base template with UI bugs

--- implementation/plaza/views.py
+++ implementation/plaza/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def base(request):
-    return render(request, 'base.html', context)
+    return render(request, 'base.html', {})
 
 ############################################## Functionality #############################################
